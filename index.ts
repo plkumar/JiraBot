01e54132a050469f2ddab1dcf0870a1dff2c0473
Lakshman Peethani <lpeethani@allianceglobalservices.com>
2016-05-29 16:09:04 +0530

    work in progress changes.

--- index.ts
+++ index.ts
@@ -3,8 +3,20 @@
 import fs = require('fs');
 import restify = require('restify');
 import builder = require('botbuilder');
+const JiraApi = require('jira').JiraApi;
 
-var model = 'https://api.projectoxford.ai/luis/v1/application?id=f8f3c825-1076-4b5e-92fb-3ce751869384&subscription-key=c2ba4a70587642b7a4cada97a40584ed';
+var config = {
+    host: "jira.allianceglobalservices.com",
+    port: "80",
+    user: "lpeethani",
+    password: "Hasini12$"
+};
+
+var jira = new JiraApi('http', config.host, config.port, config.user, config.password, '2');
+
+var luisAppId = "f8f3c825-1076-4b5e-92fb-3ce751869384";
+var luisSubscriptionKey = "c2ba4a70587642b7a4cada97a40584ed";
+var model = `https://api.projectoxford.ai/luis/v1/application?id=${luisAppId}&subscription-key=${luisSubscriptionKey}`;
 var dialog = new builder.LuisDialog(model);
 
 
@@ -14,31 +26,68 @@ var bot = new builder.BotConnectorBot({ appId: 'nzen-jirabot', appSecret: '4d5b3
 bot.configure({
     userWelcomeMessage: "Hello... Welcome.",
     goodbyeMessage: "Goodbye...",
-    groupWelcomeMessage : "Welcome."  
+    groupWelcomeMessage: "Welcome."
 });
 
 bot.add('/', dialog);
+
+dialog.onDefault(builder.DialogAction.send("I'm sorry I didn't understand."));
+
+dialog.on('GetProjects', [function (session, args, next) {
+
+    if (args.entities.length > 0) {
+        next();
+    } else {
+        jira.listProjects(function (error, result: [any]) {
+            //console.log("project", result);
+
+            var projects = [];
+            result.forEach((item) => {
+                projects.push(`[${item.key}] - ${item.name}`);
+            });
+            session.dialogData.projects = projects;
+            builder.Prompts.choice(session, "Choose project of your choice?", projects);
+        });
+    }
+},
+    function (session, results, next) {
+        console.log(results);
+        session.send("Changing project to " + results.response.entity);
+        next();
+    },
+    function (session, args) {
+        //session.send("you've chosen", args);
+        session.endDialog("bye");
+    }]);
 
 dialog.on('GetAllIssues', [
     function (session, args, next) {
         // Resolve and store any entities passed from LUIS.
         var status = builder.EntityRecognizer.findEntity(args.entities, 'issue_status');
-        var type = builder.EntityRecognizer.findEntity(args.entities,'issue_type');
-        var assignedTo = builder.EntityRecognizer.findEntity(args.entities,'assigned_to');
-        // var alarm = session.dialogData.alarm = {
-        //   title: title ? title.entity : null,
-        //   timestamp: time ? time.getTime() : null  
-        // };
-        
+        var type = builder.EntityRecognizer.findEntity(args.entities, 'issue_type');
+        var assignedTo = builder.EntityRecognizer.findEntity(args.entities, 'assigned_to');
+        var query = session.dialogData.query = {
+            status: status ? status.entity : null,
+            type: type ? type.entity : null,
+            assignedTo: assignedTo ? assignedTo.entity : null
+        };
+
         // // Prompt for title
-        // if (!alarm.title) {
-        //     builder.Prompts.text(session, 'What would you like to call your alarm?');
-        // } else {
-        //     next();
-        // }
-        console.log( status, type, assignedTo);
-        session.endDialog('Hello World!');
-    }]);
+        if (!query.type) {
+            builder.Prompts.text(session, 'What would you like to call your alarm?');
+        } else {
+            next();
+        }
+        console.log(status, type, assignedTo);
+        //session.endDialog('Hello World!');
+    },
+    function (session, results, next) {
+        console.log(results);
+    },
+    function (session, results) {
+        session.send("in next");
+    }
+]);
 
 // bot.add('/', function (session) {
 //     session.send('Hello World!');
@@ -54,8 +103,8 @@ server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
 
 // enable static route for web content
 server.get(/\/static\/?.*/, restify.serveStatic({
-  directory: __dirname + '/public',
-  default: 'index.html'
+    directory: __dirname + '/public',
+    default: 'index.html'
 }));
 
 server.listen(process.env.port || 3978, function () {
